23ff23891f3e2e0a4c2b27652312a2d47f0ed67f
NishantSambyal <nishantsambyal123@gmail.com>
2023-03-06 13:26:44 +0530

    Splash Screen Done

--- App.js
+++ App.js
@@ -1,9 +1,14 @@
 import {View, Text} from 'react-native';
-import React from 'react';
+import React, {useEffect} from 'react';
 import AppNavation from './src/route/AppNavation';
 import {Header} from './src/components';
+import SplashScreen from 'react-native-splash-screen';
 
 const App = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return (
     <View style={{flex: 1}}>
       <Header />
--- android/app/src/main/java/com/movie_app/MainActivity.java
+++ android/app/src/main/java/com/movie_app/MainActivity.java
@@ -1,9 +1,12 @@
 package com.movie_app;
 
+import android.os.Bundle;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactActivityDelegate;
+// react-native-splash-screen >= 0.3.1
+import org.devio.rn.splashscreen.SplashScreen; // here
 
 public class MainActivity extends ReactActivity {
 
@@ -15,7 +18,11 @@ public class MainActivity extends ReactActivity {
   protected String getMainComponentName() {
     return "movie_app";
   }
-
+  @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        SplashScreen.show(this);  // here
+        super.onCreate(savedInstanceState);
+    }
   /**
    * Returns the instance of the {@link ReactActivityDelegate}. Here we use a util class {@link
    * DefaultReactActivityDelegate} which allows you to easily enable Fabric and Concurrent React
