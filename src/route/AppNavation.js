83f8d3ffb99b4f40b5f11d6ae438f872c3d8d17d
NishantSambyal <nishantsambyal123@gmail.com>
2023-03-07 11:54:45 +0530

    Material Top Tabs Done

--- src/route/AppNavation.js
+++ src/route/AppNavation.js
@@ -2,13 +2,15 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import Movies from '../screens/Movies';
+import BaseMovies from '../screens/BaseMovies';
 
 const AppNavation = () => {
   const Stack = createNativeStackNavigator();
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{headerShown: false}}>
-        <Stack.Screen name="Movies" component={Movies} />
+        {/* <Stack.Screen name="Movies" component={Movies} /> */}
+        <Stack.Screen name="BaseMovies" component={BaseMovies} />
       </Stack.Navigator>
     </NavigationContainer>
   );
